577ce18d7d3e54b5463226b1bbb9f2502b65f599
phumebyte <phumelele.dlamini1000@gmail.com>
2024-08-27 16:22:25 +0200

    Added comments

--- Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
+++ Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
@@ -4,13 +4,13 @@ function validateSyntax() {
     let result = ''; // Placeholder for validation result
     // regex is a pattern used to match character combinations when processing text
     const regex = new RegExp('^pet_[a-zA-Z0-9]+$');
-
+    //if statement to output conditional logic based on result
     if (regex.test(input)) {
         result = "Valid Syntax";
     } else {
         result = "Invalid Syntax";
     }
-    
+    //fetch element by id to change the text content of result
     document.getElementById('result').innerText = result;
 }
 
